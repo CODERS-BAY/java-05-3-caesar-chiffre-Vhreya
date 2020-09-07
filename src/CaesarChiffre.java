5ec7cbdd646680b412fabf60da32146497ae95a8
Vanessa <va.au@gmx.at>
2020-09-07 09:37:31 +0200

    pos & i, operators

--- src/CaesarChiffre.java
+++ src/CaesarChiffre.java
@@ -4,33 +4,25 @@ public class CaesarChiffre {
     public static void main(String[] args) {
         Random zufall = new Random();
 
-        //das braucht sicher noch Feinschliff, aber soweit meine Lösung zu der Aufgabe:
+        //ja das macht Sinn
 
-        int pos = 0;
         String txt = "Ein Charakter namens Caesar!";
-        int rotation = zufall.nextInt(25) +1;
+        int rotation = zufall.nextInt(25) + 1;
         System.out.println(rotation);
 
         for (int i = 1; i <= txt.length(); i++) {
 
-            char letter = txt.charAt(pos);
+            char letter = txt.charAt(i-1);
+
+            // das mit den Modifikationszuweisungen hatte ich so aus dem Buch übernommen, so sieht das natürlich schöner aus xD
 
             if (letter < 65) {
             } else if (letter < 91) {
-                int alphabetStart = 'A';
-                letter -= alphabetStart;
-                letter += rotation;
-                letter %= 26;
-                letter += alphabetStart;
+                char newLetter = (char) ('A' + (letter - 'A' + rotation) % 26);
             } else {
-                int alphabetStart = 'a';
-                letter -= alphabetStart;
-                letter += rotation;
-                letter %= 26;
-                letter += alphabetStart;
+                char newLetter = (char) ('a' + (letter - 'a' + rotation) % 26);
             }
             System.out.print(letter);
-            pos++;
         }
 
 
